e65ce58e9e95ebdef5b8878fee80eefc27e862b8
slingshort <salina.myat@gmail.com>
2023-01-12 21:15:24 +1100

    hero image background for pages, slight formatting of views

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,10 +5,12 @@ import Cart from "../components/Cart";
 
 const Home = () => {
   return (
-    <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+    <div className="bg-home">
+      <div className="container">
+        <CategoryMenu />
+        <ProductList />
+        <Cart />
+      </div>
     </div>
   );
 };
