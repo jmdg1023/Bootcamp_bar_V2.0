4d289b26f63723aee65896ed2707fcb008d8405f
Astrid <jmdg1023@gmail.com>
2023-01-17 19:10:16 +1100

    fix seeds image & import models

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -25,7 +25,7 @@ db.once('open', async () => {
       name: 'Vampires Attic',
       description:
         'Black Vodka, Lemon Juice, Grape Juice, Club Soda, Dry Ice, Blackberries Garnish',
-      image: 'vampires-attic.jpg',
+      image: '/images/vampires-attic.jpg',
       category: categories[0]._id,
       price: 29,
       quantity: 1
@@ -34,7 +34,7 @@ db.once('open', async () => {
       name: 'All Greengit',
       description:
         'Finlandia Vodka, lemon juice, bergamot cordial, lemon bitter, cucumber, rosemary, leaves of basil, Egg white',
-      image: 'all-green.jpg',
+      image: '/images/all-green.jpg',
       category: categories[0]._id,
       price: 29,
       quantity: 1
@@ -44,7 +44,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Gin, fresh lemon juice, simple syrup, egg white, raspberries Garnish: pink rose',
-      image: 'clover-club.jpg',
+      image: '/images/clover-club.jpg',
       price: 19,
       quantity: 1
     },
@@ -53,7 +53,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Four Pillars Bloody Shiraz Gin, Fresh Lemon Juice, Hibiscus Syrup,Hibiscus Vanilla Ginger Brew',
-      image: 'in-my-heart.jpg',
+      image: '/images/in-my-heart.jpg',
       price: 29,
       quantity: 1
     },
@@ -62,7 +62,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Gin, Supasawa,⁣ simple syrup⁣, pine liqueur,⁣ dash woodland bitters,⁣ Electric Sky Spirulina powder⁣',
-      image: 'glacier-sour.jpg',
+      image: '/images/glacier-sour.jpg',
       price: 29,
       quantity: 1
     },
@@ -72,7 +72,7 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'butterfly pea tequila, Top with grapefruit soda, lime,',
-      image: 'butterfly-pea-paloma.jpg',
+      image: '/images/butterfly-pea-paloma.jpg',
       price: 24,
       quantity: 1
     },
@@ -81,7 +81,7 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         ' tequila blanco, lime juice syrup, triple sec, physalis berries, Rose Flower',
-      image: 'spicy-physalis-&-rose-margarita',
+      image: '/images/spicy-physalis-&-rose-margarita',
       price: 30,
       quantity: 1
     },
@@ -90,7 +90,7 @@ db.once('open', async () => {
       category: categories[3]._id,
       description:
         'The Real McCoy 3yr old Rum, Lime Juice, Simple Syrup, Blue Curacao, Mint Leaves, Soda Water',
-      image: 'monito.jpg',
+      image: '/images/monito.jpg',
       price: 29,
       quantity: 1
     },
@@ -99,7 +99,7 @@ db.once('open', async () => {
       category: categories[4]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'barrell-proof-bourbon-swizzle.jpg',
+      image: '/images/barrell-proof-bourbon-swizzle.jpg',
       price: 29,
       quantity: 20
     },
@@ -108,7 +108,7 @@ db.once('open', async () => {
       category: categories[5]._id,
       description:
         'Mezcal, Pisco, fresh lime juice, simple syrup, Egg white, cucumber slice',
-      image: 'smoke.jpg',
+      image: '/images/smoke.jpg',
       price: 19,
       quantity: 1
     },
@@ -117,7 +117,7 @@ db.once('open', async () => {
       category: categories[5]._id,
       description:
         '▪️Del Maguey Vida Mezcal,Amaro Branca Menta, AbsintheTempus Fugit White Creme De Menthe, Heavy Cream, Dark Chocolate & Mint Sprig Garnish',
-      image: 'grasshopper.jpg',
+      image: '/images/grasshopper.jpg',
       price: 29,
       quantity: 1
     }
@@ -125,7 +125,7 @@ db.once('open', async () => {
       name: 'fresh pear',
       description:
         ' Fresh Pear Juice, Lime Juice, Rose Water, Orange Bitters',
-      image: 'fresh-pear.jpg',
+      image: '/images/fresh-pear.jpg',
       category: categories[6]._id,
       price: 19,
       quantity: 1
@@ -134,7 +134,7 @@ db.once('open', async () => {
       name: 'Mind traveller',
       category: categories[7]._id,
       description: 'Yuzu Liqueur, Campari, Lemon Juice, Angostura Bitters, Aquafaba',
-      image: 'mind-traveller.jpg',
+      image: '/images/mind-traveller.jpg',
       price: 19,
       quantity: 1
     },
